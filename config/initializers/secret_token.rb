6593cf2a3f0483636910d23941662c75a2a9eeb3
Emil Pirfalt <emil.pirfalt@gmail.com>
2013-11-07 15:18:55 +0100

    Rails scafold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeSite::Application.config.secret_key_base = 'c621cbeaef22cba2963b4a6b05086a328cbd8caf7481ad59e32b73a91bf874c63ff82d54adad43eedf96ef9e12e7169e3b9d0bc2f6be956ca9a9ce9c23420a73'
