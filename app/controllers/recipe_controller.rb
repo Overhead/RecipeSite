59c223c320559b74e628677b5eccaf4596d0036e
Jon Martin <jonm@Tjarb-Tosh.(none)>
2013-11-21 12:22:35 +0100

    Worked a little on specific recipe site, and displaying search result

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,13 +1,12 @@
 class RecipeController < ApplicationController
   respond_to :xml, :json, :html
   def index
-    @recipe = Recipe.all
+    @recipes = Recipe.all
     respond_with(@recipe)
   end
   
   def show
     @recipe = Recipe.find(params[:id])
-    @ingred = Ingredient.find(1)
     respond_with(@recipe)
   end
   
